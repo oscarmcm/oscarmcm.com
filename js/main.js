8de468807d3dd0fe6e976d354fc63ebf68074665
Oscar Cortez <om.cortez.2010@gmail.com>
2015-04-02 17:23:01 -0600

    add javascript for the site

---
+++ js/main.js
@@ -0,0 +1,7 @@
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+      
