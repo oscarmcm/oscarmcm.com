28d7f5fbb4e80b840557479c5be9e10074178e27
Oscar Cortez <om.cortez.2010@gmail.com>
2015-04-04 21:03:32 -0600

    add scrollspy

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-  $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
+$('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 240
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  }
+);
       
+$(document).ready(function(){
+  $('#scrollspy').scrollSpy();
+  $('.counter').counterUp();
+});
