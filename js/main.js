c88354dc5385dc6c7eb1803ca027a8b2e9f84b48
Oscar Cortez <om.cortez.2010@gmail.com>
2015-04-06 14:06:33 -0600

    add isotope and fix side nav closes on click

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
 $('.button-collapse').sideNav({
     menuWidth: 240, // Default is 240
     edge: 'left', // Choose the horizontal origin
-    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
   }
-);
-      
+  );
+
 $(document).ready(function(){
   $('#scrollspy').scrollSpy();
   $('.counter').counterUp();
   height = $(window).height();
   $('#main').css("height", height);
+});
+
+//isotope
+$(window).load(function(){
+  var $container = $('.js-project-container');
+  $container.isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 750,
+      easing: 'linear',
+      queue: false
+    }
+  });
+
+  $('.js-project-filter a').click(function(){
+    $('.js-project-filter .current').removeClass('current');
+    $(this).addClass('current');
+
+    var selector = $(this).attr('data-filter');
+    $container.isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 750,
+        easing: 'linear',
+        queue: false
+      }
+    });
+    return false;
+  }); 
 });
