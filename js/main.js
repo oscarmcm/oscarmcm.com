8d2f88abca01a4166f55474cbb2a37ac3a236175
Oscar Cortez <om.cortez.2010@gmail.com>
2015-05-11 22:16:08 -0600

    add KONAMI code :smile:

--- js/main.js
+++ js/main.js
@@ -40,3 +40,19 @@ $(window).load(function(){
     return false;
   }); 
 });
+
+var konami_keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+var konami_index = 0;
+$(document).keydown(function(e){
+    if(e.keyCode === konami_keys[konami_index++]){
+        if(konami_index === konami_keys.length){
+            $(document).unbind('keydown', arguments.callee);
+            $.getScript('http://www.cornify.com/js/cornify.js',function(){
+                cornify_add();
+                $(document).keydown(cornify_add);
+            }); 
+        }
+    }else{
+        konami_index = 0;
+    }
+});
